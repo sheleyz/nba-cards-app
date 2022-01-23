2c4abab153d92bb5783caaa8310d727921cabdf0
sheleyz <59847602+sheleyz@users.noreply.github.com>
2022-01-22 22:10:02 -0500

    Added test player data

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,50 +1,37 @@
 const initState = {
-    products: JSON.parse(localStorage.getItem("localStore")) || [
+    players: [
         {
-            name: "Lego Superman",
-            stock: 5,
-            price: 15,
-            image: "https://images.unsplash.com/photo-1513384312027-9fa69a360337?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1771&q=80"
+            name: "Jonathan Taylor",
+            position: "RB",
+            yds: 1811,
+            td: 18,
+            avg: 5.5
         },
         {
-            name: "Lego Spaceman",
-            stock: 10,
-            price: 5,
-            image: "https://images.unsplash.com/photo-1560167016-022b78a0258e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1767&q=80"
+            name: "Aaron Rodgers",
+            position: "QB",
+            yds: 4115,
+            td: 37,
+            avg: 7.8
         },
         {
-            name: "Lego Stormtrooper",
-            stock: 15,
-            price: 10,
-            image: "https://images.unsplash.com/photo-1472457974886-0ebcd59440cc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1769&q=80"
+            name: "Cooper Kupp",
+            position: "WR",
+            yds: 1947,
+            td: 16,
+            avg: 13.4
         },
         {
-            name: "Lego Batman",
-            stock: 20,
-            price: 25,
-            image: "https://images.unsplash.com/photo-1584847642060-a46e239155a8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1769&q=80"
+            name: "Travis Kelce",
+            position: "TE",
+            yds: 1125,
+            td: 9,
+            avg: 12.2
         }
-    ],
-    cart: JSON.parse(localStorage.getItem("localCart")) || []
+    ]
 }
 
 const rootReducer = (state = initState, action) => {
-    if (action.type === 'add_product') {
-        let newProduct = state.products;
-        newProduct.push(action.product);
-        return {
-            ...state,
-            products: newProduct
-        }
-    }
-    if (action.type === 'add_to_cart') {
-        let newCartItem = state.cart;
-        newCartItem.push(action.product);
-        return {
-            ...state,
-            cart: newCartItem
-        }
-    }
     return state;
 }
 
