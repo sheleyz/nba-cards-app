f31cca7d1cbb0e0b36c8b4bd54cfc66394ccd0ef
sheleyz <59847602+sheleyz@users.noreply.github.com>
2022-04-14 21:13:45 -0400

    Added pre-loader

--- src/App.js
+++ src/App.js
@@ -6,9 +6,20 @@ import { setCards } from "./redux/reducers/setActions";
 function App() {
     const dispatch = useDispatch();
 
+    const [seconds, setSeconds] = React.useState(0);
+    const [loadCards, setLoadCards] = React.useState("load");
+    const [load, setLoad] = React.useState("load");
+
     let style = {
         display: "flex",
+        justifyContent: "center"
+    };
+
+    let loadingMessageStyle = {
+        display: "flex",
         justifyContent: "center",
+        alignItems: "center",
+        marginTop: "60px"
     };
 
     // https://www.balldontlie.io/api/v1/players?per_page=10
@@ -16,33 +27,63 @@ function App() {
 
     React.useEffect(() => {
         let players = [];
-        try {
-            for (let i = 1; i <= 5; i++) {
-                fetch(`https://www.balldontlie.io/api/v1/players?page=${i}&per_page=100`)
-                    .then(response => response.json())
-                    .then(data => {
-                        let stringifiedData = JSON.stringify(data.data);
-                        let parsedData = JSON.parse(stringifiedData);
-                        parsedData.forEach(item => players.push(item));
-                    })
-                    .then(() => {
-                        if (i >= 5) {
-                            dispatch(setCards(players));
-                            // console.log("Players:");
-                            // console.log(players);
-                        }
-                    })
-                    .then(() => console.log(`Fetched API ${i}`))
-                    .catch(() => console.log("Error fetching API"));
+        if (load === "load") {
+            try {
+                for (let i = 1; i <= 5; i++) {
+                    fetch(`https://www.balldontlie.io/api/v1/players?page=${i}&per_page=100`)
+                        .then((response) => response.json())
+                        .then((data) => {
+                            let stringifiedData = JSON.stringify(data.data);
+                            let parsedData = JSON.parse(stringifiedData);
+                            parsedData.forEach((item) => players.push(item));
+                        })
+                        .then(() => {
+                            if (i >= 5) {
+                                setLoadCards("loaded");
+                                dispatch(setCards(players));
+                                // console.log("Players:");
+                                // console.log(players);
+                            }
+                        })
+                        .then(() => console.log(`Fetched API ${i}`))
+                        .catch(() => {
+                            setLoadCards("error");
+                            console.log("Error fetching API");
+                        });
+                }
+            } catch (error) {
+                console.log("Error fetching API");
             }
-        } catch (error) {
-            console.log("Error fetching API");
         }
-    }, [dispatch]);
+
+        if (loadCards === "loaded") {
+            setLoad("loaded");
+        } else if (loadCards === "error") {
+            setLoad("error");
+        }
+
+        let interval = null;
+
+        interval = setInterval(() => {
+            setSeconds((seconds) => seconds + 1);
+        }, 500);
+
+        return () => clearInterval(interval);
+    }, [seconds, loadCards, load, dispatch]);
 
     return (
         <div className="App" style={style}>
-            <Outlet />
+            {load === "loaded" && <Outlet />}
+            {load === "load" && (
+                <div style={loadingMessageStyle}>
+                    <h1>Loading...</h1>
+                </div>
+            )}
+            {load === "error" && (
+                <div style={loadingMessageStyle}>
+                    <h1>Error Loading API. Please try again later.</h1>
+                </div>
+            )}
         </div>
     );
 }
